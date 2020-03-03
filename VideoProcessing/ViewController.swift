2c6b9375a9b249cf82529271c3a0a7c052535463
Giang Tran <giang.tran@2359media.com>
2020-03-03 22:22:49 +0700

    Initial Commit

---
+++ VideoProcessing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoProcessing
+//
+//  Created by Tran Thi Cam Giang on 3/3/20.
+//  Copyright © 2020 Tran Thi Cam Giang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
